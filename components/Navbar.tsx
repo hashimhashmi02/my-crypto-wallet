2af969b2a2fe806aaedd3b191c2f5de8fbc5ff77
hashimhashmi02 <hashimhashmi02@gmail.com>
2025-08-08 21:35:07 +0530

    changed size

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <nav className="flex justify-between items-center py-4">
       <div className="flex items-center gap-2">
-        <Box className="size-8" />
+        <Box className="size-7" />
         <span className="tracking-tighter text-3xl font-extrabold text-primary">
         Saifu
         </span>
